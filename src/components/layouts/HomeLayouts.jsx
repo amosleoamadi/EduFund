b8c59b605e78ab7d1133259678012fc8646949f7
amosleoamadi <amadiamos146@gmail.com>
2025-10-20 01:47:17 -1100

    updated the route

--- src/components/layouts/HomeLayouts.jsx
+++ src/components/layouts/HomeLayouts.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import { Footer, Header } from "../static";
 
 const HomeLayouts = () => {
-  return <div>HomeLayouts</div>;
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
 
 export default HomeLayouts;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Register from "../auth/Register";
 import Login from "../auth/Login";
+import HomeLayouts from "../components/layouts/HomeLayouts";
+import Homepage from "../pages/Homepage/Homepage";
 
 export const Element = createBrowserRouter([
   {
     path: "/",
+    element: <HomeLayouts />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+    ],
+  },
+  {
+    path: "/register",
     element: <Register />,
   },
   {
