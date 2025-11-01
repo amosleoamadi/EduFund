0106e5e02d19ee7d744f1cd839b073391408ff68
Luxurybarbie <jonahokam@gmail.com>
2025-11-01 19:26:32 +0100

    donation with few update on my donor chart

--- src/pages/donor-dashboard/components/Donor_Chart.jsx
+++ src/pages/donor-dashboard/components/Donor_Chart.jsx
@@ -1,28 +1,22 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Tooltip
-} from 'chart.js';
+import {Chart as ChartJS, CategoryScale,LinearScale,BarElement,Tooltip} from 'chart.js';
 
-import { FaChartBar } from 'react-icons/fa';
+import { FaRegChartBar } from "react-icons/fa";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip);
 
 const Donor_Chart = () => {
   const chartRef = useRef(null);
-  const [gradient, setGradient] = useState('#3b82f6'); 
+  const [gradient, setGradient] = useState('#2B7FFF66'); 
 
   useEffect(() => {
     const chart = chartRef.current;
     if (chart) {
       const ctx = chart.ctx;
       const grad = ctx.createLinearGradient(0, 0, 0, chart.height);
-      grad.addColorStop(0, '#3b82f6');
-      grad.addColorStop(1, 'rgba(59, 130, 246, 0)');
+      grad.addColorStop(0, '#1A3DE9');
+      grad.addColorStop(1, '#2B7FFF66');
       setGradient(grad);
     }
   }, []);
@@ -34,9 +28,10 @@ const Donor_Chart = () => {
         data: [60, 30, 80, 100, 40, 70],
         backgroundColor: gradient,
         borderRadius: 8,
-        barThickness: 100,
-        categoryPercentage: 0.6,
-        maxBarThickness: 100
+        barThickness: 130,
+        categoryPercentage: 0.5,
+        maxBarThickness: 150
+        
       }
     ]
   };
@@ -62,13 +57,13 @@ const Donor_Chart = () => {
   };
 
   return (
-    <div className="chart_card">
+    <div style={{boxShadow: " rgba(0, 0, 0, 0.16) 0px 1px 4px", borderRadius: "25px", padding: "2rem"}}>
       <div className="donation-header">
         <div>
           <h3 style={{fontSize: "22px", fontWeight: 400}}>Donation History</h3>
           <p style={{fontSize: "16px", lineHeight: "1.5]" }}>Your giving pattern over the last 6 months.</p>
         </div>
-        <FaChartBar style={{fontSize: "25px", fontWeight: 200}} />
+        <FaRegChartBar style={{fontSize: "25px", fontWeight: 100, color: "#99A1AF"}} />
       </div>
       <Bar ref={chartRef} data={data} options={options} />
     </div>
